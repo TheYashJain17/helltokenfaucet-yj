f945e973ea45c26755234e443384ab7959f52e44
yash jain <yashjain07.yj@gmail.com>
2023-07-12 16:44:58 +0530

    Configure Our Hardhat.config File To Deploy Our Token Contract On Mumbai Polygon Network.

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,8 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+
+const polygonUrl = process.env.POLYGON_URL;
+
+const account = process.env.PRIVATE_KEY;
+
+
 module.exports = {
+  
   solidity: "0.8.17",
+
+  networks : {
+
+    mumbai : {
+
+      url : `${polygonUrl}`,
+      accounts : [account]
+
+    }
+
+  },
 
   paths : {
 
